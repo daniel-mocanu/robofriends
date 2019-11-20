fee16e4ae2ccda3fc08390dbff7f9cbcaaeea98f
Daniel Mocanu <danielmocanu3@gmail.com>
2019-11-20 11:30:08 -0500

    Updates

---
+++ precache-manifest.2d592444befe6e5741a62959017bedab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc52bc67dc77250fa42c4fd33bc48db",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "987312c143005444774d",
+    "url": "/robofriends/static/css/main.ec27805b.chunk.css"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/robofriends/static/js/2.d75c0efc.chunk.js"
+  },
+  {
+    "revision": "987312c143005444774d",
+    "url": "/robofriends/static/js/main.1945d95b.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  }
+]);
